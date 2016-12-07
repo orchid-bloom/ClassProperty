3181f02205db78f26b323489dcbd74d843221f8f
TemaSir <tianxin@gsoft.cc>
2016-12-07 09:13:36 +0800

    Initial Commit

---
+++ ClassProperty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClassProperty
+//
+//  Created by tianXin on 2016/12/7.
+//  Copyright © 2016年 tianXin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ClassProperty/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ClassProperty
+//
+//  Created by tianXin on 2016/12/7.
+//  Copyright © 2016年 tianXin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
